8c1db91365461c1463c2b6b38270f865b9b56a86
WinterKwon <winterkwon7017@gmail.com>
2022-11-23 01:10:05 +0900

    [feat] user entity 생성

---
+++ server/src/entities/user.entity.ts
@@ -0,0 +1,41 @@
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, ObjectIdColumn, ObjectID } from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  BASIC_USER = 'basic',
+  GHOST = 'ghost',
+}
+
+export enum UserStatus {
+  NORMAL = 'normal',
+  EXPIRED = 'expired',
+  EXPELLED = 'expelled',
+}
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  //MongoDB
+  // @ObjectIdColumn()
+  // id: ObjectID;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.BASIC_USER })
+  role: UserRole;
+
+  @Column({ type: 'enum', enum: UserStatus, default: UserStatus.NORMAL })
+  userStatus: UserStatus;
+
+  @Column({ nullable: true })
+  tribe: string;
+}
